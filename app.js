c0bf4bd4113fbc319a840eab24b70d26b9204d0b
Vinicius Costa <91564654+VininCosta@users.noreply.github.com>
2022-07-28 13:58:35 -0300

    1

---
+++ app.js
@@ -0,0 +1,136 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { config } = require('dotenv');
+
+const app = express();
+
+app.use(express.json());
+
+const User = require('./models/User')
+
+//Rota aberta
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Bem vindo a API'  })
+})
+
+//Rota Privada
+app.get('/user/:id', checkToken, async (req, res) => {
+    const id = req.params.id
+
+    const user = await User.findById(id, "-password")
+    
+    if(!user) {
+        res.status(404).json({message: 'Usuário não encontrado'})
+    }
+        res.status(200).json({user})
+})
+
+function checkToken(req, res, next) {
+    const authHeader = req.headers['Authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if(!token) {
+        res.status(401).json({message: 'Acesso negado!'})
+    }
+
+    try {
+        const secret = process.env.SECRET
+        jwt.verify(token, secret)
+        next()
+    } catch (error) {
+        res.status(400).json({message: 'Token inválido'})
+    }
+}
+
+app.post('/auth/register', async(req, res) => {
+    const {nome, email, password, confirmpassword} = await req.body
+    if(!nome) {
+        res.status(422).json({message: "Nome é obrigatório"})
+    }
+    if(!email) {
+        res.status(422).json({message: "Email é obrigatório"})
+    }       
+    if(!password) {
+        res.status(422).json({message: "Senha é obrigatória"})
+    }
+    if(password !== confirmpassword) {
+        res.status(422).json({message: "Senhas não conferem."})
+    }
+
+    const userExists = await User.findOne({email: email})
+    if(userExists) {
+        res.status(422).json({message: "Por Favor, utilize outro email."})
+    }
+
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    const user = new User ({
+        nome,
+        email,
+        password: passwordHash,
+    })
+
+    try {
+        await user.save()
+        res.status(201).json({message: "Usuário criado com sucesso."})
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({message: 'Aconteceu um erro no servidor, tente novamente!',
+    })
+    }
+})
+
+app.post('/auth/login', async (req, res) => {
+    const {email, password} = req.body
+
+    if(!email) {
+        res.status(422).json({message: "Email é obrigatório"})
+    }       
+    if(!password) {
+        res.status(422).json({message: "Senha é obrigatória"})
+    }
+
+    // Checar se usuário existe
+    const user = await User.findOne({email: email})
+
+    if(!user) {
+        res.status(404).json({message: "Usuário não encontrado!"})
+    }
+
+    //Checar senha
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if(!checkPassword) {
+        res.status(422).json({message: "Senha inválida"})
+    }
+
+    try {
+        const secret = process.env.SECRET
+
+        const token = jwt.sign({
+            id: user._id,
+        },
+        secret,
+        )
+        res.status(200).json({message: "Autenticação realizada com sucesso", token})
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({message: 'Aconteceu um erro no servidor, tente novamente!'})
+    }
+})
+
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+mongoose.connect((`mongodb+srv://${dbUser}:${dbPassword}@cluster-jwt.13ff6.mongodb.net/?retryWrites=true&w=majority`))
+.then(() => {
+    app.listen(3000)
+    console.log('Banco de Dados conectado.')
+})
+.catch((err) => {
+    console.log(err)
+})
